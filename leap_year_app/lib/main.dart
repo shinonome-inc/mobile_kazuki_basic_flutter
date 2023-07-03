4184e88b11eda97555d4a10e4636533766a1f624
kazuki0615 <kzk0615kzk@gmail.com>
2023-07-03 14:17:21 +0900

    閏年判定アプリ作成

--- leap_year_app/lib/main.dart
+++ leap_year_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter　Demo',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: '判定所'),
     );
